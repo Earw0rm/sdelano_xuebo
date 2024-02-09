ba2891ee9173d5b3cbcc30f7867a381774407ce9
Earw0rm <va9im.shat0v@gmail.com>
2024-02-09 14:00:31 +0300

    start rethinking of process and kernel entry/exit

--- include/sched.h
+++ include/sched.h
@@ -4,9 +4,92 @@
 
 
 #define CPU_CONTEXT_OFFSET 0
-
+#define TRAPFRAME_OFFSET 104
 
 #ifndef __ASSEMBLER__
+
+
+struct context {
+    uint64_t x19; // 0
+    uint64_t x20; // 8
+    uint64_t x21; // 16
+    uint64_t x22; // 24
+    uint64_t x23; // 32
+    uint64_t x24; // 40
+    uint64_t x25; // 48
+    uint64_t x26; // 56
+    uint64_t x27; // 64
+    uint64_t x28; // 72
+    uint64_t x29_fp; // 80 // frame pointer
+    uint64_t x30_lr; // 88 // link register
+    uint64_t x31_sp; // 96 // stack pointer for current thread    
+};
+
+struct trapframe{
+    uint64_t sp_el0;
+	uint64_t elr_el1; 
+	uint64_t spsr_el1;
+
+    uint64_t x0;
+    uint64_t x1;
+    uint64_t x2;
+    uint64_t x3;
+    uint64_t x4;
+    uint64_t x5;
+    uint64_t x6;
+    uint64_t x7;
+    uint64_t x8;
+    uint64_t x9;
+    uint64_t x10;
+    uint64_t x11;
+    uint64_t x12;
+    uint64_t x13;
+    uint64_t x14;
+    uint64_t x15;
+    uint64_t x16;
+    uint64_t x17;
+    uint64_t x18;
+    //callee safe
+    uint64_t x19;
+    uint64_t x20;
+    uint64_t x21;
+    uint64_t x22;
+    uint64_t x23;
+    uint64_t x24;
+    uint64_t x25;
+    uint64_t x26;
+    uint64_t x27;
+    uint64_t x28;
+    uint64_t x29_fp; // frame pointer
+    uint64_t x30_lr; // link register
+    uint64_t x31_sp; // stack pointer for current thread    
+};
+
+enum procstate{
+    RUNNING, ZOMBIE 
+};
+
+// per-process state(beta)
+struct proc{
+    struct trapframe * trapframe; // 104
+    enum procstate state;         // 280
+    int64_t preempt_count;
+    int64_t xstate;
+    int64_t pid;
+
+    struct proc * parent;
+};
+
+// per-cpu state (beta)
+struct cpu{
+    struct context context;
+    struct proc * proc; // 104 
+};
+
+//TODO implement
+// extern struct cpu cpus[NCPU];
+
+
 
 #include "common.h"
 #define THREAD_SIZE 4096
@@ -56,6 +139,9 @@ struct task_struct{
     // (for example, it runs the scheduling function.). 
     // If timer tick occurs at such time it is ignored and rescheduling is not triggered.
     int64_t preempt_count;
+
+    uint64_t stack;
+    uint64_t flags;
 };
 
 // After the kernel startup, there is only one task running:
